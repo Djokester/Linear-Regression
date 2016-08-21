147659f0f0d861a3357d960d78e0999632cb8177
Djokester <samriddhidjokestersinha@gmail.com>
2016-08-22 01:35:42 +0530

    First Test Commit

--- Simple_Gradient_Descent.py
+++ Simple_Gradient_Descent.py
@@ -1,3 +1,21 @@
 import numpy as np
-import matplot.pyplot as plt
+import matplotlib.pyplot as plt
 import functools as fn
+
+#Define any Function under the function method.
+#By Default I have set it to function(X)=[x^2,y^2,z^2]. Where X = [x,y,z]. 
+def function(X): 
+    return(np.array([x**2 for x in X]))
+    
+def partial_derivative_estimate(f, X, i):
+    h=10**(-7)    
+    Xplus = [xplus + (h if j==i else 0) for j,xplus in enumerate(X)]
+    fplus=(f(Xplus))
+    fo=f(X)
+    return((fplus-fo)/h)
+
+def gradient_estimate():
+    
+X = [1,2,3]
+print(function(X))   
+print(partial_derivative_estimate(function, X, 1))
