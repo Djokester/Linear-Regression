85804a3d183d107d8fb8684a5ce42685ba5ddf99
Djokester <samriddhidjokestersinha@gmail.com>
2016-08-22 23:54:31 +0530

    SimpleGradient Descent Commit 1

--- Simple_Gradient_Descent.py
+++ Simple_Gradient_Descent.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import functools as fn
-
+import random 
 #Define any Function under the function method.
 #By Default I have set it to function(X)=[x^2,y^2,z^2]. Where X = [x,y,z]. 
 def function(X): 
-    return(np.array([x**2 for x in X]))
+    return(sum([x**2 for x in X]))
     
 def partial_derivative_estimate(f, X, i):
     h=10**(-7)    
@@ -14,8 +14,20 @@ def partial_derivative_estimate(f, X, i):
     fo=f(X)
     return((fplus-fo)/h)
 
-def gradient_estimate():
+def gradient_estimate(f, X):
+    return([partial_derivative_estimate(f, X, i) for i in range(0,len(X))])
     
-X = [1,2,3]
-print(function(X))   
-print(partial_derivative_estimate(function, X, 1))
+def step(X,gradient,h):
+    return([y-x*h for y,x in zip(X,gradient)])
+    
+step_multiplier = 0.00001
+v = [random.randint(-10,10) for i in range(3)]
+tolerance=0.000001
+while True:
+    gradient = gradient_estimate(function, v)
+    next_v = step(v,gradient,0.001)
+    if sum([(x-y)**2 for x,y in zip(v, next_v)]) < tolerance**2:
+            break
+    v = next_v
+    
+print(v)
